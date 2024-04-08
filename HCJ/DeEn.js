3611e6c8e85c1b500d6118d0fa25a8f12cf9b398
Imemyslf <kishanbro9666@gmail.com>
2024-04-09 01:27:50 +0530

    crypt finished

--- HCJ/DeEn.js
+++ HCJ/DeEn.js
@@ -78,8 +78,80 @@ function Forward(){
   }
 }
 
-function showSlide(slideId) {
-const slide = document.getElementById(slideId);
-// Scroll to the slide
-slide.scrollIntoView({ behavior: 'smooth' });
+let array;
+
+function encrypt(){
+  var text = document.getElementById('encrypt');
+  var message = text.value;
+  console.log(message);
+  let message_1 = message.split('');
+  array = new Array(message_1.length).fill('');
+  console.log(message_1);
+  
+  message_1.forEach((element, index) => {
+    let asciivalue = message_1[index].charCodeAt(0); 
+    console.log(asciivalue); 
+    array[index] = String.fromCharCode(asciivalue + 100);    
+  });
+  console.log(array);
+  array = array.join('');
+  randomchar(3);
+
+  text.value = '';
+  display_encrypt();
 }
+
+function randomchar(string_len){
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+  var random_str_1 = '';
+  var random_str_2 = '';
+
+  for (var i, i = 0;  i < string_len; i++){
+    random_str_1 += chars.charAt(Math.floor(Math.random() *  chars.length)); 
+    random_str_2 += chars.charAt(Math.floor(Math.random() *  chars.length)); 
+  }
+  array = random_str_1 + array + random_str_2;
+  console.log(array);
+}
+
+function display_encrypt(){
+  var dis_text = document.getElementById('decrypt');
+  dis_text.textContent = array;
+  
+}
+
+// let array;
+
+function decrypt(){
+  var text = document.getElementById('decrypt');
+  var message = text.value;
+  message = message.slice(3,[(message.length)-3]);
+  console.log(message);
+  let message_1 = message.split('');
+  array = new Array(message_1.length).fill('');
+  console.log(message_1);
+  
+  message_1.forEach((element, index) => {
+    let asciivalue = message_1[index].charCodeAt(0); 
+    console.log(asciivalue); 
+    array[index] = String.fromCharCode(asciivalue - 100);    
+  });
+  console.log(array);
+  array = array.join('');
+  // randomchar(3);
+
+  text.value = '';
+  display_decrypt();
+}
+
+function display_decrypt(){
+  var dis_text = document.getElementById('encrypt');
+  dis_text.value = array;
+
+}
+
+// function showSlide(slideId) {
+// const slide = document.getElementById(slideId);
+// slide.scrollIntoView({ behavior: 'smooth' });
+// event.preventDefault();
+// }
